e6489cd9aba54f83ad1bf7206c01759fe56284d5
Enes <121495299+enesarafatoglu@users.noreply.github.com>
2024-06-12 14:17:35 +0300

    Update and rename odev10.sql to odev10.md

--- odev10.sql
+++
@@ -1,8 +0,0 @@
-SELECT city.city, country.country FROM country
-LEFT JOIN city ON city.country_id = country.country_id;
-
-SELECT customer.first_name, customer.last_name, payment.payment_id FROM customer
-RIGHT JOIN payment ON payment.customer_id = customer.customer_id;
-
-SELECT customer.first_name, customer.last_name, rental.rental_id FROM customer
-FULL JOIN rental ON customer.customer_id = rental.customer_id;
