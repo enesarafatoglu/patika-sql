fd540c4e2bc07794e3b6ab2f499f931efcdc5f8d
Enes <121495299+enesarafatoglu@users.noreply.github.com>
2024-06-12 13:35:23 +0300

    Create odev12.sql

---
+++ odev12.sql
@@ -0,0 +1,14 @@
+SELECT COUNT(*) FROM film
+WHERE length > (SELECT AVG(length) FROM film);
+
+SELECT COUNT(*) FROM film
+WHERE rental_rate  = (SELECT MAX(rental_rate) FROM film);
+
+SELECT title, rental_rate, replacement_cost FROM film
+WHERE rental_rate = (SELECT MIN(rental_rate) FROM film) AND replacement_cost = (SELECT MIN(replacement_cost) FROM film);
+
+SELECT customer.customer_id, customer.first_name, customer.last_name, COUNT(payment.payment_id) AS total_payments
+FROM customer
+INNER JOIN payment ON customer.customer_id = payment.customer_id
+GROUP BY customer.customer_id, customer.first_name, customer.last_name
+ORDER BY total_payments DESC;
